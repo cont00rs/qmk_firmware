5fcf9dc402c72781274a29f7660191c549805f9d
cont00rs <cont00rs@users.noreply.github.com>
2024-05-03 07:38:58 +0200

    Add dedicated key for CAPS_WORD

--- keyboards/splitkb/kyria/keymaps/cont00rs/rules.mk
+++ keyboards/splitkb/kyria/keymaps/cont00rs/rules.mk
@@ -3,3 +3,4 @@ ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no    # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 SEND_STRING_ENABLE = yes  # allow to send multiple strings per key
+CAPS_WORD_ENABLE = yes
